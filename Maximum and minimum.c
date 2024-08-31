9aae63b0e34e31739dc7460232d20c066f995339
Amith0-alex <amithpraveen9201@gmail.com>
2024-08-31 13:55:44 +0530

    Create Maximum and minimum.c

---
+++ Maximum and minimum.c
@@ -0,0 +1,21 @@
+include<stdio.h>
+void main()
+{
+  int a[10],i,n,min,max;
+  printf("enter the size of array");
+  scanf("%d",&n);
+  printf("enter the elements of array");
+  for(i=0;i<n;i++)   
+    scanf("%d",&a[i]);
+  max=a[0];
+  min=a[0];
+  for(i=1;i<n;i++)
+    {
+      if(a[i]>max)
+        max=a[i];
+      else if(a[i]<min)
+        min=a[i];
+    }
+  printf("max=%d\n",max);
+  printf("min=%d\n",min);
+  printf("\nalex ");
